5e38ce031afdfa7867a90793d75a29561cceca8b
Jack Robbers <jack@2pisoftware.com>
2020-03-02 16:58:07 +1100

    add motor driver

---
+++ motor_driver/motor_driver.ino
@@ -0,0 +1,15 @@
+#define enA 9
+#define in1 6
+#define in2 7
+
+void setup() {
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+}
+
+void loop() {
+  analogWrite(enA, 80);
+}
