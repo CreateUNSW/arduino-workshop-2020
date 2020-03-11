80b9bd5b5330be51f5fb66cbb4a81043ed5e544e
Jack Robbers <jack@2pisoftware.com>
2020-03-11 17:11:16 +1100

    pre workshop 2 changes

--- motor_driver/motor_driver.ino
+++ motor_driver/motor_driver.ino
@@ -1,15 +1,16 @@
 #define enA 9
-#define in1 6
-#define in2 7
+#define in1 8
+#define in2 7 
 
 void setup() {
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  analogWrite(enA, 200);
 }
 
 void loop() {
-  analogWrite(enA, 255);
+//   analogWrite(enA, );
 }
